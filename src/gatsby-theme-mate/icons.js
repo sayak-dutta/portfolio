677cfde73cdbcc73f4e928cc98eabce1d348a909
sayak-dutta <dsayak29@gmail.com>
2021-08-24 00:04:24 +0530

    Updates

--- src/gatsby-theme-mate/icons.js
+++
@@ -1,4 +0,0 @@
-import { faInstagram, faTwitter, faGithub } from "@fortawesome/free-brands-svg-icons";
-import { faGlobe, faEnvelope, faCoffee } from "@fortawesome/free-solid-svg-icons";
-
-export const ICONS = [faInstagram, faTwitter, faGithub, faGlobe, faEnvelope, faCoffee];
